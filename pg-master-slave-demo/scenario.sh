6bb23657e7752610346aaa0a5f6323aa58a7795e
Thijs Lemmens <thijs.lemmens@xenit.eu>
2018-01-07 14:34:42 +0100

    Added master-slave scenario

---
+++ pg-master-slave-demo/scenario.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#Starting the setup with 1 master
+docker-compose up -d
+
+#Setting up the slave: Uncomment slave in compose file
+docker-compose up -d
+
+#Killing the master
+docker-compose stop pgmaster
+
+#Check if Alfresco works (read only)
+#Promote the slave to master
+docker exec --user=postgres pgmasterslavedemo_pgslave_1 pg_ctl promote
