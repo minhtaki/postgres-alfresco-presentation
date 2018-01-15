65edae8723c1f47cc2184e9ba27cf4bafa95e9ce
Thijs Lemmens <thijs.lemmens@xenit.eu>
2018-01-15 12:05:29 +0100

    Simplify and fix PITR demo

--- pg-pitr-demo/scenario.sh
+++ pg-pitr-demo/scenario.sh
@@ -4,36 +4,33 @@
 #start everything
 docker-compose up -d
 
-## start pgrecovery (uncomment)
-docker-compose up -d
-
 #check if base backup succeeded, then stop pgrecovery
 docker-compose stop pgrecovery
 
 # Add document
 # Record time
 docker exec --user=postgres pgpitrdemo_postgres_1 psql -c "SELECT NOW();"
+# 2018-01-15 10:55:49.866192+00
 
 # Remove document, also from trash
-# stop Alfresco
-docker-compose stop alfresco-core
-docker-compose rm alfresco-core
 
 # edit recovery.conf
 #restore_command = 'cp /wal_archive/%f %p'
 #recovery_target_time = '2018-01-09 09:39:50'
 #recovery_target_action = promote
 
-#copy and wal files
-docker exec -it pgpitrdemo_postgres_1 bash
-cp /var/lib/postgresql/data/PGDATA/pg_xlog/* /wal_archive/
-chown -R postgres:postgres /wal_archive
+#show the xlog files
+docker exec -it --user=postgres pgpitrdemo_postgres_1 ls /wal_archive
 
+#force wal file switch
+docker exec -it --user=postgres pgpitrdemo_postgres_1 psql -c 'SELECT pg_switch_xlog()'
+
+#show the xlog files again
+docker exec -it --user=postgres pgpitrdemo_postgres_1 ls /wal_archive
 
 #start pgrecovery
 docker-compose start pgrecovery
 #check if the recovery went fine
 
-#change alfresco jdbc config
-#start alfresco
-docker-compose up -d
+#stop master postgres to force switch
+docker-compose stop postgres
